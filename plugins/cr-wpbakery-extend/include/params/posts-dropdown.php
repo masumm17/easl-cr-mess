210cf6bbc8e01c522d41e261e1b8a807655d9697
Mahbub Hasan <mmhasaneee@gmail.com>
2018-07-13 21:40:45 +0600

    Custom Param: Posts Dropdown - check for empty posts list

--- plugins/cr-wpbakery-extend/include/params/posts-dropdown.php
+++ plugins/cr-wpbakery-extend/include/params/posts-dropdown.php
@@ -21,6 +21,11 @@ function cr_vce_param_posts_dropdown( $settings, $value ) {
 		'hierarchical' => 1,
 		'post_type' => $post_type,
 	));
+	if( !$dropdown ||  (false === strpos($dropdown, 'wpb_vc_param_value' )) ){
+		$dropdown = "<select name='{$settings['param_name']}' class='wpb_vc_param_value wpb-input wpb-select {$settings['param_name']} {$settings['type']}' id='{$settings['param_name']}'>";
+		$dropdown .= '<option value="">'. $empty_option .'</option>';
+		$dropdown .= '</select>';
+	}
 	return $dropdown;
 
 }
