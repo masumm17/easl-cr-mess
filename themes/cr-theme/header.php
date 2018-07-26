6be69d469de5625199f27ee6dab20d88e656ef62
Mahbub Hasan <mmhasaneee@gmail.com>
2018-07-26 14:51:42 +0600

    IE changes

--- themes/cr-theme/header.php
+++ themes/cr-theme/header.php
@@ -26,44 +26,45 @@
 		<img src="<?php echo esc_url($preloaer_image); ?>" alt="<?php _e('Loading...', 'crt'); ?>"/>
 	</div>
 	<?php endif; ?>
-	<div id="cr-wrap">
-		<header id="masthead" class="site-header">
-			<div class="site-branding">
-				<a class="site-logo" href="<?php echo home_url( '/' ); ?>" title="">
-					<img class="site-main-logo" alt="" src="<?php echo crt_header_logo_img(); ?>"/>
-					<img class="scroll-header-logo" alt="" src="<?php echo crt_scroll_header_logo_img(); ?>"/>
-				</a>
-			</div>
-			<nav class="site-primary-navigation">
-				<?php 
-				wp_nav_menu( array(
-					'theme_location' => 'header_menu',
-					'menu_class'     => 'header-primary-menu',
-					'container'      => false,
-					'fallback_cb'    => false,
-					'link_before'    => '<span class="link-inner">',
-					'link_after'     => '</span>',
-					'walker'		 => new CR_Dropdown_Walker_Nav_Menu,
-				) );
-				?>
-			</nav>
+	
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<a class="site-logo" href="<?php echo home_url( '/' ); ?>" title="">
+				<img class="site-main-logo" alt="" src="<?php echo crt_header_logo_img(); ?>"/>
+				<img class="scroll-header-logo" alt="" src="<?php echo crt_scroll_header_logo_img(); ?>"/>
+			</a>
+		</div>
+		<nav class="site-primary-navigation">
 			<?php 
-			$header_highlight_button = crt_header_highlighted_butotn();
-			if($header_highlight_button):
+			wp_nav_menu( array(
+				'theme_location' => 'header_menu',
+				'menu_class'     => 'header-primary-menu',
+				'container'      => false,
+				'fallback_cb'    => false,
+				'link_before'    => '<span class="link-inner">',
+				'link_after'     => '</span>',
+				'walker'		 => new CR_Dropdown_Walker_Nav_Menu,
+			) );
 			?>
-			<div class="section-header-highlight">
-				<?php echo $header_highlight_button; ?>
-			</div>
-			<?php endif; ?>
-			<?php 
-			if(crt_sticky_nav_enabled()) {
-				get_template_part('modules/stikcy-side-navigation');
-			}
-			?> 
-			<?php if(defined('ICL_SITEPRESS_VERSION')):?>
-			<div class="mobile-language-selector">
-				<?php echo do_shortcode('[wpml_language_switcher][/wpml_language_switcher]'); ?>
-			</div>
-			<?php endif; ?>
-		</header>
+		</nav>
+		<?php 
+		$header_highlight_button = crt_header_highlighted_butotn();
+		if($header_highlight_button):
+		?>
+		<div class="section-header-highlight">
+			<?php echo $header_highlight_button; ?>
+		</div>
+		<?php endif; ?>
+		<?php 
+		if(crt_sticky_nav_enabled()) {
+			get_template_part('modules/stikcy-side-navigation');
+		}
+		?> 
+		<?php if(defined('ICL_SITEPRESS_VERSION')):?>
+		<div class="mobile-language-selector">
+			<?php echo do_shortcode('[wpml_language_switcher][/wpml_language_switcher]'); ?>
+		</div>
+		<?php endif; ?>
+	</header>
+	<div id="cr-wrap">
 		<div id="primary" class="content-area">
