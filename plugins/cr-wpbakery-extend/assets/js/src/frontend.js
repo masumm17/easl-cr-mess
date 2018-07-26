453d05f2dc8c3ee63f1369dd2ad1901329e53dfd
Mahbub Hasan <mmhasaneee@gmail.com>
2018-07-26 14:51:13 +0600

    Map changes for IE11

--- plugins/cr-wpbakery-extend/assets/js/src/frontend.js
+++ plugins/cr-wpbakery-extend/assets/js/src/frontend.js
@@ -27,6 +27,45 @@ if (!String.prototype.padStart) {
     };
 }
 (function($){
+    function crDetectIE() {
+        var ua = window.navigator.userAgent;
+
+        // Test values; Uncomment to check result …
+
+        // IE 10
+        // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+
+        // IE 11
+        // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+
+        // Edge 12 (Spartan)
+        // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+
+        // Edge 13
+        // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+
+        var msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+          // IE 10 or older => return version number
+          return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        }
+
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+          // IE 11 => return version number
+          var rv = ua.indexOf('rv:');
+          return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+
+        var edge = ua.indexOf('Edge/');
+        if (edge > 0) {
+          // Edge (IE 12+) => return version number
+          return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        }
+
+        // other browser
+        return false;
+    }
     function escapeHtml(string) {
         var entityMap = {
           "&": "&amp;",
@@ -855,12 +894,16 @@ if (!String.prototype.padStart) {
                     }
                   ]
             });
+            var isIE = !!crDetectIE();
             for(var i = 0; i < mapData.markers.length; i++) {
+                var iconImg = false;
+                iconImg = isIE ? {url: mapData.markers[i].icon, scaledSize: new google.maps.Size(54, 54)} : mapData.markers[i].icon;
                 var marker = new google.maps.Marker({
                     map: map,
                     draggable: false,
                     position: {lat: mapData.markers[i].lat, lng: mapData.markers[i].lng},
-                    icon: mapData.markers[i].icon,
+                    icon: iconImg,
+                    optimized: false,
                     zIndex: parseInt(mapData.markers[i].order)
                 });
                 marker.crMapID = mapID;
