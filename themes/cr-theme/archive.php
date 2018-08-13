ccc2ff52434d6a92dda83103081105de8712770a
Mahbub Hasan <mmhasaneee@gmail.com>
2018-08-13 18:44:52 +0600

    Develop archieve page.

--- themes/cr-theme/archive.php
+++ themes/cr-theme/archive.php
@@ -10,9 +10,7 @@
 get_header();
 
 // Get Blog page template
-if( is_home() && !is_front_page() ) {
-	get_template_part( 'template-parts/blog' );
-}
+get_template_part( 'template-parts/blog' );
 
 get_footer();
 ?>
